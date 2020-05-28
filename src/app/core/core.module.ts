ea1d9eac557a62f39f848764aaf8822948859117
TomaszOrpik <orpik.tomasz@gmail.com>
2020-05-28 19:46:14 +0200

    Adding src folder

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([])
+  ],
+  declarations: [
+    BsNavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    ContactComponent,
+  ],
+  exports: [
+    BsNavbarComponent
+  ]
+})
+export class CoreModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA1g5TKehkP2vu6GdU8WO5DQgtsP3e1-YQ",
+    authDomain: "vegeshop-714fb.firebaseapp.com",
+    databaseURL: "https://vegeshop-714fb.firebaseio.com",
+    projectId: "vegeshop-714fb",
+    storageBucket: "vegeshop-714fb.appspot.com",
+    messagingSenderId: "222555639384",
+    appId: "1:222555639384:web:7dbdca3c2698e3af90c42d"
+  }
+};
